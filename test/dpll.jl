128f9debfd33240da71ba6eeb5236a971cbe6890
Tung Anh Vu <vu.tunganh96@gmail.com>
2020-07-06 18:27:08 +0200

    Test if returned satisfying assignment satisfies all clauses

--- test/dpll.jl
+++ test/dpll.jl
@@ -1,13 +1,23 @@
-import SatSolver: dpll, brute_force, is_formula_satisfiable
+import SatSolver: dpll, brute_force, is_formula_satisfiable, SatSolverResult
 
 @testset "DPLL algorithm" begin
+  function dpll_test(fla::Formula, ref::SatSolverResult)
+    dpll_res = dpll(fla)
+    correct_result = is_formula_satisfiable(dpll_res) == is_formula_satisfiable(ref)
+    if correct_result && is_formula_satisfiable(ref)
+      @test correct_result && all((cl) -> is_clause_satisfied(cl, dpll_res.satisfying_assignment), fla)
+    else
+      @test correct_result
+    end
+  end
+
   @testset "Random formulas" begin
     for _ in 1:500
       fla = gen_rand_formula(;
                              max_clauses=Unsigned(50),
                              max_variables=Unsigned(20),
                              max_clause_len=Unsigned(4))
-      @test is_formula_satisfiable(dpll(fla)) == is_formula_satisfiable(brute_force(fla))
+      dpll_test(fla, brute_force(fla))
     end
   end
 end
